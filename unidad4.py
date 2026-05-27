44de01a58a67d631c771907cf5ffc85456262731
reyesyubana92-pixel <reyesyubana92@gmail.com>
2026-05-27 14:07:24 -0600

    Add files via upload

---
+++ unidad4.py
@@ -0,0 +1,4 @@
+muestras = (22, 25, 22, 26, 22, 23, 24)
+
+frecuencia_22 = muestras.count(22)
+print(f"La temperatura de 22°C se registró {frecuencia_22} veces.")
---
+++ unidad7.py
@@ -0,0 +1,7 @@
+laboratorio = set()
+
+laboratorio.add("Carlos")
+laboratorio.add("Ana")
+
+laboratorio.discard("Pedro")
+print(f"Estudiantes actuales en el laboratorio: {laboratorio}")
